d24b7c86d46c5475affe93c3a44731db07f77bff
Francisco <francisco.morao04@gmail.com>
2023-12-01 17:07:37 +0000

    W

--- Projeto1_base/main.c
+++ Projeto1_base/main.c
@@ -46,7 +46,6 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Failed to initialize EMS\n");
     return 1;
   }
-
   while (1) {
     unsigned int event_id, delay;
     size_t num_rows, num_columns, num_coords;
@@ -55,10 +54,17 @@ int main(int argc, char *argv[]) {
     while ((dp = readdir(dirp)) != NULL) {
       if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
         continue;
+      char *out = strstr(dp->d_name, ".out");
+      if (out!=NULL && (strcmp(out, ".out") == 0))
+        continue;
       //printf("%s\n",dp->d_name);//nao esta alfabeticamente ordenado
-      char *filename = path;
+      char *filename = (char *) malloc(strlen(path) + strlen("/") + strlen(dp->d_name) + 1); //erro para alocar memoria
+      strcpy(filename,path);
       strcat(filename,"/");
       strcat(filename,dp->d_name);
+      
+      
+      // criar um cadeia de caracteres onde o .jobs e trocado por .out para escrever o output do ficheiro lido
       size_t replace_str_len = strlen("jobs");
       size_t replace_with_len = strlen("out");
       size_t modified_len = strlen(dp->d_name) - replace_str_len + replace_with_len;
@@ -68,9 +74,12 @@ int main(int argc, char *argv[]) {
       strncpy(file_out_name, dp->d_name, copy_length);
       file_out_name[copy_length] = '\0';
       strcat(file_out_name, "out");
-      strcat(file_out_name, found_position + replace_str_len);
-      printf("%s\n",file_out_name);
-      printf("%s\n",filename);
+      char *filename_out = (char *) malloc(strlen(path) + strlen("/") + strlen(file_out_name) + 1);
+      strcpy(filename_out,path);
+      strcat(filename_out,"/");
+      strcat(filename_out,file_out_name);
+      /*printf("%s\n",filename);
+      printf("%s\n",filename_out);*/
       int file = open(filename,O_RDONLY);
       if (file == -1) {
         write(STDERR_FILENO, "Error opening file\n", 19);
@@ -78,11 +87,14 @@ int main(int argc, char *argv[]) {
       }
 
       int filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-      int file_out = open(file_out_name, O_CREAT | O_TRUNC | O_WRONLY , filePerms);
+      int file_out = open(filename_out, O_CREAT | O_TRUNC | O_WRONLY , filePerms);
       if (file_out == -1) {
         write(STDERR_FILENO, "Error opening file\n", 19);
         exit(EXIT_FAILURE);
       }
+      free(filename);
+      free(file_out_name);
+      free(filename_out);
       fflush(stdout);
       int i;
       while ((i =  get_next(file)) >= 0){
@@ -171,132 +183,19 @@ int main(int argc, char *argv[]) {
             return 0;
         }
       }
-      free(file_out_name);
       if (close(file) == -1){
+        printf("2");
         write(STDERR_FILENO, "Error closing file\n", 20);
         exit(EXIT_FAILURE);
       }
         
       if (close(file_out) == -1){
+        printf("1");
         write(STDERR_FILENO, "Error closing file\n", 20);
         exit(EXIT_FAILURE);
       }
-        //errExit("close input");*/
-      }
+    }
     break;
-    
-    /*char buffer[100] = "/home/francisco/SO/SO_projeto_1/Projeto1_base/jobs/test.jobs\0";
-    int file = open(buffer,O_RDONLY);
-    if (file == -1) {
-      write(STDERR_FILENO, "Error opening file\n", 19);
-      exit(EXIT_FAILURE);
-    }
-    char *file_out_name = (char*) malloc(sizeof(char) * strlen(buffer));
-    strncpy(file_out_name, buffer, strlen(buffer)- strlen("jobs\0"));
-    strcat(file_out_name, "out\0");
-    int filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
-      S_IROTH | S_IWOTH;
-    int file_out = open(file_out_name, O_CREAT | O_TRUNC | O_WRONLY , filePerms);
-    if (file_out == -1) {
-      write(STDERR_FILENO, "Error opening file\n", 19);
-      exit(EXIT_FAILURE);
-    }
-    fflush(stdout);
-    int i;
-    while ((i =  get_next(file)) >= 0){
-      switch (i) {
-        case CMD_CREATE:
-          if (parse_create(file, &event_id, &num_rows, &num_columns) != 0) {
-            fprintf(stderr, "Invalid command. See HELP for usage\n");
-            continue;
-          }
-
-          if (ems_create(event_id, num_rows, num_columns)) {
-            fprintf(stderr, "Failed to create event\n");
-          }
-
-          break;
-
-        case CMD_RESERVE:
-          num_coords = parse_reserve(file, MAX_RESERVATION_SIZE, &event_id, xs, ys);
-
-          if (num_coords == 0) {
-            fprintf(stderr, "Invalid command. See HELP for usage\n");
-            continue;
-          }
-
-          if (ems_reserve(event_id, num_coords, xs, ys)) {
-            fprintf(stderr, "Failed to reserve seats\n");
-          }
-
-          break;
-
-        case CMD_SHOW:
-          if (parse_show(file, &event_id) != 0) {
-            fprintf(stderr, "Invalid command. See HELP for usage\n");
-            continue;
-          }
-
-          if (ems_show(file_out, event_id)) {
-            fprintf(stderr, "Failed to show event\n");
-          }
-
-          break;
-
-        case CMD_LIST_EVENTS:
-          if (ems_list_events()) {
-            fprintf(stderr, "Failed to list events\n");
-          }
-
-          break;
-
-        case CMD_WAIT:
-          if (parse_wait(file, &delay, NULL) == -1) {  // thread_id is not implemented
-            fprintf(stderr, "Invalid command. See HELP for usage\n");
-            continue;
-          }
-
-          if (delay > 0) {
-            printf("Waiting...\n");
-            ems_wait(delay);
-          }
-
-          break;
-
-        case CMD_INVALID:
-          fprintf(stderr, "Invalid command. See HELP for usage\n");
-          break;
-
-        case CMD_HELP:
-          printf(
-              "Available commands:\n"
-              "  CREATE <event_id> <num_rows> <num_columns>\n"
-              "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
-              "  SHOW <event_id>\n"
-              "  LIST\n"
-              "  WAIT <delay_ms> [thread_id]\n"  // thread_id is not implemented
-              "  BARRIER\n"                      // Not implemented
-              "  HELP\n");
-
-          break;
-
-        case CMD_BARRIER:  // Not implemented
-        case CMD_EMPTY:
-          break;
-
-        case EOC:
-          ems_terminate();
-          return 0;
-      }
-    }
-    if (close(file) == -1){
-      write(STDERR_FILENO, "Error closing file\n", 20);
-      exit(EXIT_FAILURE);
-    }
-      
-    if (close(file) == -1){
-      write(STDERR_FILENO, "Error closing file\n", 20);
-      exit(EXIT_FAILURE);}
-      //errExit("close input");*/
+    closedir(dirp);
   }
 }
