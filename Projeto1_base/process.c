27c156c7706147824d4520b03e92b1c4f2c608df
Francisco <francisco.morao04@gmail.com>
2023-12-06 23:38:16 +0000

    com mae
    Co-authored-by: miguel1099 <miguel1099@users.noreply.github.com>

--- Projeto1_base/process.c
+++ Projeto1_base/process.c
@@ -14,6 +14,7 @@
 #include "process.h"
 
 pthread_mutex_t lock;
+int end_command;
 int comando;
 
 void* thread(void* arg){
@@ -21,18 +22,20 @@ void* thread(void* arg){
     unsigned int event_id, delay;
     size_t num_rows, num_columns, num_coords;
     size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
-    if ((comando = get_next(valores->file)) < 0)
+    /*if ((comando = get_next(valores->file)) < 0)
     {
-       /*Da ERRO*/
-    }
+        //da erro
+    }*/
     //int* command = malloc(sizeof(int));
     //command = &valores->command;
     //printf("valores.command dentro: %d\n", valores->command);
+    valores->command = get_next(valores->file);
+    comando = valores->command;
     if (pthread_mutex_init(&lock, NULL) != 0) {
        fprintf(stderr, "Failed to initialize the lock\n");
        exit(EXIT_FAILURE);
     }
-    switch (comando) {
+    switch (valores->command) {
     case CMD_CREATE:
         //printf("Create : %d\n", valores->command);
         if (parse_create(valores->file, &event_id, &num_rows, &num_columns) != 0) {
@@ -47,9 +50,8 @@ void* thread(void* arg){
         return NULL;
     }
     pthread_mutex_lock(&lock);
-    switch (comando) {
+    switch (valores->command) {
     case CMD_RESERVE:
-        
         num_coords = parse_reserve(valores->file, MAX_RESERVATION_SIZE, &event_id,xs,ys);
         if (num_coords == 0) {
             fprintf(stderr, "Invalid command. See HELP for usage\n");
@@ -106,6 +108,7 @@ void* thread(void* arg){
       break;
     case EOC:
         pthread_mutex_lock(&lock);
+        end_command = 9;
         ems_terminate();
         if (close(valores->file) == 1){
             write(STDERR_FILENO, "Error closing file\n", 20);
@@ -122,11 +125,74 @@ void* thread(void* arg){
     return NULL;
 }
 
-/*void* thread_mom(){
-
-}*/
+void* thread_mom(void* arg){
+    data *values = (data*) arg;
+    int number_threads = 0;
+    int i = 0;
+    int k = 0;
+    data valores[values->max_threads];
+    pthread_t thread_id[values->max_threads];////alterar indice que controla
+    while (1){
+      //printf("%d\n",comando);
+      //pthread_t thread_id[i];
+      //void* return_value;
+        //printf("%d\n",comando);
+        if(number_threads >= values->max_threads){ //(i%max_threads)
+            k = (k + 1)%values->max_threads;//indice que controla indice da primeira thread criada ainda ativa (//if(i==k)tinha acho desnecessario)
+            //printf("indice join%d\n", i);
+            if (pthread_join(thread_id[i], NULL) != 0){
+                fprintf(stderr, "Failed to join1 thread\n");
+                exit(EXIT_FAILURE);
+            }
+            number_threads--;
+        }
+        //valores->command = comando;
+        valores[i].file = values->file;
+        valores[i].file_out = values->file_out;
+        //valores[i].command = get_next(values->file);
+        if (comando == EOC) break;
+        if (pthread_create(&thread_id[i], NULL, &thread, &valores[i]) != 0){
+            fprintf(stderr, "Failed to create thread\n");
+            exit(EXIT_FAILURE);
+        }
+        //printf("indice create%d %d\n", i, k);
+        //int* comando = (int*)return_value;
+        if (comando == EOC) break;
+        //FILHO DE PAIS MATEMATICOS
+        number_threads++; //max=3 (0,1,2)3
+        i = (i + 1)%values->max_threads;
+        //free(comando);
+        //free(valores);
+    }
+    while(number_threads > 0){
+        //printf("indice join 2_%d\n", k);
+        if (pthread_join(thread_id[k], NULL) != 0){
+            fprintf(stderr, "Failed to join2 thread\n");
+            exit(EXIT_FAILURE);
+        }
+        k = (k + 1)%values->max_threads;
+        number_threads--;
+    }
+    return NULL;
+}
 
 void read_files(char* path, char* name, int max_threads){
+    pthread_t thread_id;
+    int file = open_file_read(path, name);
+    int file_out = open_file_out(path, name);
+    data valor;
+    valor.file = file;
+    valor.file_out = file_out;
+    valor.max_threads = max_threads;
+    if (pthread_create(&thread_id, NULL, &thread_mom, &valor) != 0){
+        fprintf(stderr, "Failed to create thread\n");
+        exit(EXIT_FAILURE);
+    }
+    if (pthread_join(thread_id, NULL) != 0){
+        fprintf(stderr, "Failed to join1 thread\n");
+        exit(EXIT_FAILURE);
+    }
+    return;
     //pthread_t thread_id;
     /*
     pthead mae le cm 
@@ -178,50 +244,4 @@ void read_files(char* path, char* name, int max_threads){
         if (comando == EOC) break;
     
     */
-    int number_threads = 0;
-    int i = 0;
-    int k = 0;
-    int file = open_file_read(path, name);
-    int file_out = open_file_out(path, name);
-    data valores[max_threads];
-    pthread_t thread_id[max_threads];////alterar indice que controla
-    while (1){
-      //printf("%d\n",comando);
-      //pthread_t thread_id[i];
-      //void* return_value;
-        if(number_threads >= max_threads){ //(i%max_threads)
-            k = (k + 1)%max_threads;//indice que controla indice da primeira thread criada ainda ativa (//if(i==k)tinha acho desnecessario)
-            //printf("indice join%d\n", i);
-            if (pthread_join(thread_id[i], NULL) != 0){
-            fprintf(stderr, "Failed to join1 thread\n");
-            exit(EXIT_FAILURE);
-            }
-            number_threads--;
-        }
-        //valores->command = comando;
-        valores[i].file = file;
-        valores[i].file_out = file_out;
-        if (comando == EOC) break;
-        if (pthread_create(&thread_id[i], NULL, &thread, &valores[i]) != 0){
-            fprintf(stderr, "Failed to create thread\n");
-            exit(EXIT_FAILURE);
-        }
-        //printf("indice create%d %d\n", i, k);
-        //int* comando = (int*)return_value;
-        if (comando == EOC) break;
-        //FILHO DE PAIS MATEMATICOS
-        number_threads++; //max=3 (0,1,2)3
-        i = (i + 1)%max_threads;
-        //free(comando);
-        //free(valores);
-    }
-    while(number_threads > 0){
-        //printf("indice join 2_%d\n", k);
-        if (pthread_join(thread_id[k], NULL) != 0){
-            fprintf(stderr, "Failed to join2 thread\n");
-            exit(EXIT_FAILURE);
-        }
-        k = (k + 1)%max_threads;
-        number_threads--;
-    }
 }
--- Projeto1_base/process.h
+++ Projeto1_base/process.h
@@ -2,11 +2,14 @@
 #define PROCESS_H
 
 #include <stddef.h>
+#include "parser.h"
 #include "constants.h"
 
 typedef struct{
   int file;
   int file_out;
+  int command;
+  int max_threads;
 }data;
 
 void read_files(char* path, char* name , int max_threads);
