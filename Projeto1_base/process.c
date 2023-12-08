06f547175a13a33df85796d7d047f8cd870ae6eb
Francisco <francisco.morao04@gmail.com>
2023-12-08 14:29:03 +0000

    novos testes
    Co-authored-by: miguel1099 <miguel1099@users.noreply.github.com>

--- Projeto1_base/process.c
+++ Projeto1_base/process.c
@@ -33,24 +33,12 @@ void* thread(void* arg){
     unsigned int event_id, delay;
     size_t num_rows, num_columns, num_coords;
     size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
-    /*if ((comando = get_next(valores->file)) < 0)
-    {
-        //da erro
-    }*/
-    //int* command = malloc(sizeof(int));
-    //command = &valores->command;
-    //printf("valores.command dentro: %d\n", valores->command);
-    /*if (pthread_mutex_init(&lock, NULL) != 0) {
-       fprintf(stderr, "Failed to initialize the lock\n");
-       exit(EXIT_FAILURE);
-    }*///Ba 12 22 2 2 
-    printf("comando : %d\n", comando);
     while (comando != EOC){
         //printf("valores->command:%d\n",valores->command);
         //pthread_mutex_lock(&lock);
         pthread_mutex_lock(&file_lock);
         valores->command = get_next(valores->file);
-        printf("Create : %d\n", valores->command);
+        //printf("Create : %d\n", valores->command);
         //barrier_wait(0);
         
         active_threads++;
@@ -60,23 +48,26 @@ void* thread(void* arg){
             if (parse_create(valores->file, &event_id, &num_rows, &num_columns) != 0) {
                 fprintf(stderr, "Invalid command. See HELP for usage\n");
                 //free(arg);
-                return NULL;
+                pthread_mutex_unlock(&file_lock);
+                break;
+                //return NULL;
             }
             //pthread_mutex_unlock(&lock);
-            pthread_mutex_unlock(&file_lock);
             if (ems_create(event_id, num_rows, num_columns)) {
                 fprintf(stderr, "Failed to create event\n");
             }
+            
+            pthread_mutex_unlock(&file_lock);
             break;
-            //free(arg);
             //return NULL;
         //pthread_mutex_lock(&lock);
         case CMD_RESERVE:
             num_coords = parse_reserve(valores->file, MAX_RESERVATION_SIZE, &event_id,xs,ys);
             if (num_coords == 0) {
                 fprintf(stderr, "Invalid command. See HELP for usage\n");
-                //free(arg);
-                return NULL;
+                pthread_mutex_unlock(&file_lock);
+                break;
+                //return NULL;
             }
             //pthread_mutex_unlock(&lock);
             pthread_mutex_unlock(&file_lock);
@@ -87,8 +78,9 @@ void* thread(void* arg){
         case CMD_SHOW:
             if (parse_show(valores->file, &event_id) != 0) {
                 fprintf(stderr, "Invalid command. See HELP for usage\n");
-                //free(arg);
-                return NULL;
+                pthread_mutex_unlock(&file_lock);
+                break;
+                //return NULL;
             }
             if (ems_show(valores->file_out, event_id)) {
                 fprintf(stderr, "Failed to show event\n");
@@ -108,7 +100,7 @@ void* thread(void* arg){
             //int *thread_id;
             if (parse_wait(valores->file, &delay, NULL) == -1) {  // thread_id is not implemented
                 fprintf(stderr, "Invalid command. See HELP for usage\n");
-                //free(arg);
+                pthread_mutex_unlock(&file_lock);
                 return NULL;
             }
             //pthread_mutex_unlock(&lock);
@@ -146,25 +138,15 @@ void* thread(void* arg){
         case EOC:
             //pthread_mutex_lock(&lock);
             if (comando == EOC) return NULL;
-            
             comando = EOC;
             /*barrier = 1;
             barrier_wait(1);*/
             ems_terminate();
-            /*if (close(valores->file) == 1){
-                write(STDERR_FILENO, "Error closing file\n", 20);
-                exit(EXIT_FAILURE);
-            }
-            if (close(valores->file_out) == 1){
-                write(STDERR_FILENO, "Error closing file\n", 20);
-                exit(EXIT_FAILURE);
-            }*/
             //pthread_mutex_unlock(&lock);
             pthread_mutex_unlock(&file_lock);
             return NULL;
         }
         active_threads--;
-        //free(arg);
         //pthread_mutex_unlock(&lock);
         
     }
