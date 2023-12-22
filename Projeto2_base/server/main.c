1e81753e238f8c8cd15cd4aad7bb58dfece4f2b5
Francisco <francisco.morao04@gmail.com>
2023-12-22 22:27:01 +0000

    iniciai so algumas coisa ve comentarios

--- Projeto2_base/server/main.c
+++ Projeto2_base/server/main.c
@@ -1,6 +1,11 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include "common/constants.h"
 #include "common/io.h"
@@ -11,8 +16,9 @@ int main(int argc, char* argv[]) {
     fprintf(stderr, "Usage: %s\n <pipe_path> [delay]\n", argv[0]);
     return 1;
   }
-
+  char *pipe_name = "";
   char* endptr;
+  int fcli, fserv;
   unsigned int state_access_delay_us = STATE_ACCESS_DELAY_US;
   if (argc == 3) {
     unsigned long int delay = strtoul(argv[2], &endptr, 10);
@@ -29,15 +35,42 @@ int main(int argc, char* argv[]) {
     fprintf(stderr, "Failed to initialize EMS\n");
     return 1;
   }
-
   //TODO: Intialize server, create worker threads
+  /*
+  COM ISTO JA TEMOS O SERVIDOR INICIALIZADO FALTA AS WORKER THREADS
+  PARA ISSO ACHO QUE TEMOS DE:
+    QUANDO QUERIAMOS UMA NOVO CLIENTE CRIASSE TAMBEM UMA THREAD NOVA QUE 
+    FICA ASSOCIADA A ESSE CLINTE
+  */
+  pipe_name = argv[1];
+  unlink(pipe_name);
+  if (unlink(pipe_name) != 0 && errno != ENOENT) {
+    fprintf(stderr, "unlink failed\n");
+    exit(EXIT_FAILURE);
+  }
+  if (mkfifo(pipe_name, 0777) < 0){
+    fprintf(stderr, "mkfifo failed\n");
+    exit(EXIT_FAILURE);
+  } 
+
+  fserv = open(pipe_name, O_RDONLY);
+  if (fserv == -1) {
+    fprintf(stderr, "Server open failed\n");
+    exit(EXIT_FAILURE);
+  }
 
   while (1) {
+    /*OK JA PERCEBI NO API NOS CRIAMOS O PIPE DO CLINTE
+    DENTRO DA FUNÇAO DO EMS_SETUP VAI ESCREVER NO PIPE_NAME 
+    DESTE LADO LEMOS MEIO QUE INSCREVEMOS O DUDE NO SERVER */
+    
     //TODO: Read from pipe
+
     //TODO: Write new client to the producer-consumer buffer
   }
 
   //TODO: Close Server
-
+  close(fserv);
+  unlink(pipe_name);
   ems_terminate();
 }
